b7e5b320adcb5a3e138c1c24dc9228be990cca0d
wigarwijaya <wigarwijaya@gmail.com>
2022-10-14 21:41:00 +0700

    delet node version

--- app.js
+++ app.js
@@ -138,5 +138,5 @@ app.get("/about", (req, res) => {
 // });
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log(`Server has started successfully.`);
 });
